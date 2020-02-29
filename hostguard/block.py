f4ba2b283e22765032f65c151952aab53519e024
Sam Ireland <sam.ireland.uk@gmail.com>
2020-02-29 15:09:12 +0000

    Temporary unblocking

--- hostguard/block.py
+++ hostguard/block.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from time import sleep
 from hosts import get_host_lines, save_host_lines
 
 HOST_LOCATION = "/etc/hosts"
@@ -10,6 +11,14 @@ if len(sys.argv) < 2:
     print("What hostname should be blocked?")
     sys.exit()
 domain_to_block = sys.argv[1]
+
+if len(sys.argv) > 2:
+    try:
+        time_to_sleep = float(sys.argv[2])
+        sleep(time_to_sleep * 60)
+    except:
+        print("Couldn't parse number of seconds")
+        sys.exit()
 
 host_contents = get_host_lines(HOST_LOCATION)
 
@@ -20,3 +29,4 @@ if new_line not in host_contents:
 save_host_lines(HOST_LOCATION, host_contents)
 
 print("Blocked %s" % domain_to_block)
+sys.exit()
--- hostguard/unblock.py
+++ hostguard/unblock.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import subprocess
 from hosts import get_host_lines, save_host_lines
 
 HOST_LOCATION = "/etc/hosts"
@@ -19,4 +20,15 @@ host_contents = [line for line in host_contents\
 save_host_lines(HOST_LOCATION, host_contents)
 
 print("Unblocked %s" % domain_to_unblock)
+
+if len(sys.argv) > 2:
+    path = os.path.dirname(os.path.realpath(__file__))
+    command = "python %s%sblock.py %s %s > /dev/null 2>&1 &" % (
+        path, os.path.sep, domain_to_unblock, sys.argv[2]
+    )
+    os.system(command)
+    try:
+        float(sys.argv[2])
+        print("Will block it again in %s minutes" % sys.argv[2])
+    except: pass
 
